cd1402e587f335a4eda7a7876a6fc9549343ce26
tasnimbensalam <123754612+tasnimbensalam@users.noreply.github.com>
2024-11-11 00:21:59 +0100

    first

---
+++ app/src/main/java/com/example/movieclub/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.movieclub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+//variables
+    Animation topAnim,botomAnim;
+    ImageView  image;
+    TextView slogan;
+    private static int SPLASH_SCREEN=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        botomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        image=findViewById(R.id.image);
+
+        slogan=findViewById(R.id.slogan);
+        image.setAnimation(topAnim);
+        slogan.setAnimation(botomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, login.class));
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+
+    }
