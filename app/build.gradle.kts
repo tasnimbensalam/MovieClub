75e768c4858432bfbc91110177dcd133ece05038
tasnimbensalam <123754612+tasnimbensalam@users.noreply.github.com>
2024-11-11 22:32:41 +0100

    ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
