f14334bc30d61f42ea6bc343bec457196339c56a
tasnimbensalam <123754612+tasnimbensalam@users.noreply.github.com>
2024-11-11 21:11:50 +0100

    added register

--- app/src/main/java/com/example/movieclub/Register.java
+++ app/src/main/java/com/example/movieclub/Register.java
@@ -5,27 +5,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
+    private Button callSignIn;
 
-    private Button callSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        callSignUp = findViewById(R.id.signup);
-        callSignUp.setOnClickListener(view -> {
-            Intent intent = new Intent(login.this, Register.class);
+        callSignIn = findViewById(R.id.signin);
+        callSignIn.setOnClickListener(view -> {
+            Intent intent = new Intent(Register.this, login.class);
             startActivity(intent);
         });
+
     }
 }
